8109036860018e78ae4a1fb5b770fc361485ea83
Srikant <chaudharysrikant26@gmail.com>
2023-07-20 20:49:02 +0530

    Adding mergeSort

--- Recursion/reverseAnumber.cpp
+++ Recursion/reverseAnumber.cpp
@@ -29,9 +29,10 @@ bool pal(int arr[],int l,int r)
 int main() {
     // Write C++ code here
     cout << "Hello world!";
-    int arr[9]={1,2,3,4,5,6,7,8,9};
+//    int arr[9]={1,2,3,4,5,6,7,8,9};
 //    reverse(arr,0,9);
-	cout<<bool(pal(arr,0,9));
+	String arr
+	cout<<pal(arr,0,9);
 //    for(int i=0;i<9;i++)
 //    {
 //        cout<<arr[i]<<" ";
